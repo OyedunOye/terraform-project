b8204051e7efe042a0a4b6b100fe28db90db51db
OyedunOye <oyesinaoyedun@yahoo.com>
2026-09-05 21:03:09 +0200

    clean up the main config file.

--- main.tf
+++ main.tf
@@ -24,30 +24,11 @@ resource "aws_subnet" "my-app-subnet-1" {
     }
 }
 
-# explicit creation of another route table which will not only have the local auto created route, but also the internet gateway route for internet access to the vpc
-# resource "aws_route_table" "my-app-route-table" {
-#     vpc_id = aws_vpc.my-app-vpc.id
-#     route {
-#         cidr_block = "0.0.0.0/0"
-#         gateway_id = aws_internet_gateway.my-app-igw.id
-#     }
-
-#     tags = {
-#         Name: "${var.env_prefix}-rtb"
-#     }
-# }
-
-# creates an internet gateway that is associated to the external route in explicitly created route table
+# creates an internet gateway that is associated to the external route in default created route table
 resource "aws_internet_gateway" "my-app-igw" {
     vpc_id = aws_vpc.my-app-vpc.id
 
 }
-
-# links route table to the created subnet, must be defined explicitly only for route table which is not main
-# resource "aws_route_table_association" "a-rtb-subnet" {
-#     subnet_id = aws_subnet.my-app-subnet-1.id
-#     route_table_id = aws_route_table.my-app-route-table.id
-# }
 
 # instead of creating another route table, use the default created on vpc creation and add igw to it
 resource "aws_default_route_table" "main-rtb" {
@@ -63,9 +44,8 @@ resource "aws_default_route_table" "main-rtb" {
     }
 }
 
-# this is where incoming (ingress) and outgoing (egress) traffic via ports are defined for server instance level.
-resource "aws_security_group" "my-app-sg" {
-    name = "my-app-sg"
+# this is where incoming (ingress) and outgoing (egress) traffic via ports are defined for server instance level. This modifies the default sg in the current vpc
+resource "aws_default_security_group" "default-sg" {
     vpc_id = aws_vpc.my-app-vpc.id
 
     ingress {
@@ -91,15 +71,6 @@ resource "aws_security_group" "my-app-sg" {
     }
 
     tags = {
-        Name: "${var.env_prefix}-sg"
+        Name: "${var.env_prefix}-default-sg"
     }
 }
-
-# define output in one per output block as follows
-# output "my-dev-subnet-id" {
-#     value = aws_subnet.my-app-subnet-1.id
-# }
-
-# output "my-dev-vpc-id" {
-#     value = aws_vpc.my-app-vpc.id
-# }
