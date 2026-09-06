f0ed5d1a7bbd6d3488285b61ab16efb1340d05af
OyedunOye <oyesinaoyedun@yahoo.com>
2026-09-06 23:57:10 +0200

    add config for ec2 instance and connect machine generated public ssh key for secure login to the instance.

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip_range" {}
+variable "instance_type" {}
+variable "public_ssh_key_location" {}
 
 resource "aws_vpc" "my-app-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -27,7 +29,9 @@ resource "aws_subnet" "my-app-subnet-1" {
 # creates an internet gateway that is associated to the external route in default created route table
 resource "aws_internet_gateway" "my-app-igw" {
     vpc_id = aws_vpc.my-app-vpc.id
-
+    tags = {
+        Name: "${var.env_prefix}-igw"
+  }
 }
 
 # instead of creating another route table, use the default created on vpc creation and add igw to it
@@ -52,7 +56,7 @@ resource "aws_default_security_group" "default-sg" {
         from_port = 22
         to_port = 22
         protocol = "TCP"
-        cidr_blocks = var.my_ip_range
+        cidr_blocks = [var.my_ip_range]
     }
 
     ingress {
@@ -73,4 +77,48 @@ resource "aws_default_security_group" "default-sg" {
     tags = {
         Name: "${var.env_prefix}-default-sg"
     }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["al2023-ami-2023*x86_64"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+# using an existing ssh key pair on my computer
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_ssh_key_location)
+
+}
+
+resource "aws_instance" "my-app-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.my-app-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
+}
+
+output "ec2-public_ip" {
+  value = aws_instance.my-app-server.public_ip
 }
