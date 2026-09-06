2d6234381ef464f3d96712f430157f8b2ab0e05e
OyedunOye <oyesinaoyedun@yahoo.com>
2026-09-07 01:31:07 +0200

    add a script that runs once on instance provision or whenever the script content itself changes with 'user_data' attribute.
    it installs docker and runs an nginx container.

--- main.tf
+++ main.tf
@@ -114,6 +114,15 @@ resource "aws_instance" "my-app-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
+    user_data = <<-EOF
+                    #!/bin/bash
+                    sudo yum update -y && sudo yum install -y docker
+                    sudo systemctl start docker
+                    sudo usermod -aG docker ec2-user
+                    docker run -p 8080:80 nginx
+                EOF
+
+    user_data_replace_on_change = true
     tags = {
         Name: "${var.env_prefix}-server"
     }
